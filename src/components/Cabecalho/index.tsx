f6ed08a287567671f4646d9db1252ac9e2b29222
David T. <davidt.info@gmail.com>
2025-05-03 19:13:36 -0300

    first commit

---
+++ src/components/Cabecalho/index.tsx
@@ -0,0 +1,12 @@
+import logo from './assets/logo.png'
+import perfil from './assets/perfil.png'
+
+function Cabecalho() {
+  return (
+    <header>
+        
+    </header>
+  )
+}
+
+export default Cabecalho
